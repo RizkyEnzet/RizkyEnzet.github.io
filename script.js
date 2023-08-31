1c2ef7aa74e367d12b7e5d441d9b4b1ea285dcf0
RizkyEnzet <143733914+RizkyEnzet@users.noreply.github.com>
2023-08-31 22:40:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+function redirInstagram() {
+    window.location.href = "http://https://www.instagram.com/rizkynrzhn17"
+}
+function redirWhatsApp() {
+    window.location.href ="wa.me/+6289518793436"
+}
+</script>
